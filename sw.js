484c30fc9132ffd64daf8f7116607464b9f550b4
joekpolison-prog <joekpolison@gmail.com>
2026-09-08 00:23:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const CACHE = "pwabuilder-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+// Ditukar kepada index.html supaya aplikasi anda boleh dibuka secara offline
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
